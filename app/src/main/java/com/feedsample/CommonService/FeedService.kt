7a5423c5b1b33e1b6128c627358d78b746f97d6a
Gurvinder Singh <gurvinder.singh@nactus.com>
2020-09-14 13:09:36 +0530

    Retrofit builder intialization in FeedService class

--- app/src/main/java/com/feedsample/CommonService/FeedService.kt
+++ app/src/main/java/com/feedsample/CommonService/FeedService.kt
@@ -3,6 +3,7 @@ package com.feedsample.CommonService
 
 import androidx.lifecycle.MutableLiveData
 import com.assignment.kotlinmvvm.DataModel.FeedModel
+import com.feedsample.interfaces.ApiInterface
 import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.Call
@@ -12,7 +13,22 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class FeedService {
 
+    val liveUserResponse: MutableLiveData<FeedModel> = MutableLiveData()
 
+    companion object Factory {
+        const val BASE_URL = "https://dl.dropboxusercontent.com/"
+        var gson = GsonBuilder().setLenient().create()
+        fun create(): ApiInterface {
+
+            val retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .baseUrl(BASE_URL)
+                .build()
+
+            return retrofit.create(ApiInterface::class.java)
+        }
+    }
 
     fun loadFeedData(): MutableLiveData<FeedModel>? {
 
