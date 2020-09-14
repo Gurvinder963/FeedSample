b18dacd97f5f41c749cc462463bf85d4f444709c
Gurvinder Singh <gurvinder.singh@nactus.com>
2020-09-14 12:10:07 +0530

    Commiting feed response model.

---
+++ app/src/main/java/com/feedsample/DataModel/FeedModel.kt
@@ -0,0 +1,8 @@
+package com.assignment.kotlinmvvm.DataModel
+
+class FeedModel(
+     val title:String,
+     val rows:List<FeedRowsModel>)
+
+
+
---
+++ app/src/main/java/com/feedsample/DataModel/FeedRowsModel.kt
@@ -0,0 +1,9 @@
+package com.assignment.kotlinmvvm.DataModel
+
+data class FeedRowsModel(
+
+   val title:String,
+   val description:String,
+   val imageHref:String)
+
+
